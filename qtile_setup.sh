baaf9e920f8abf64a74f852178d2e34d1270f32c
NevilleJS <nevillejs24@gmail.com>
2022-02-18 09:52:59 +0530

    bruh

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions
-yay -S picom-jonaburg-git rofi-greenclip ly nerd-fonts-jetbrains-mono 
+sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions lightdm lightdm-webkit-theme-litarvan lightdm-webkit2-greeter light-locker udiskie lxappearance gsfonts
+yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono 
 
 chsh -s /usr/bin/zsh
 
-sudo systemctl start ly
-sudo systemctl enable ly
+sudo systemctl enable lightdm
+sudo systemctl enable udisks2
+
+sudo cp -r ~/.config/misc_configs/etc/* /etc/
+sudo cp -r ~/.config/misc_configs/usr/* /usr/
+sudo cp ~/Pictures/Wallpapers/wallpaper.png /usr/share/backgrounds/
+
+sudo lxappearance
+lxappearance
