3e8fd65a37b9b516e92d9728d2957ae46f448206
NevilleJS <nevillejs24@gmail.com>
2022-02-18 08:15:20 +0530

    sldfjk

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -2,3 +2,8 @@
 
 sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions
 yay -S picom-jonaburg-git rofi-greenclip ly nerd-fonts-jetbrains-mono 
+
+chsh -s /usr/bin/zsh
+
+sudo systemctl start ly
+sudo systemctl enable ly
