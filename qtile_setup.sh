55233e327d6f48553d3cd1667fa9abb49028fafb
NevilleJS <nevillejs24@gmail.com>
2022-02-24 15:32:26 +0530

    burh

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock
+sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock bluez bluez-utils pulseaudio-bluetooth blueman
 yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly
 
 chsh -s /usr/bin/zsh
 
 sudo systemctl enable ly
 sudo systemctl enable udisks2
+sudo systemctl enable bluetooth.service
 
 sudo cp -r ~/.config/misc_configs/etc/* /etc/
 sudo cp -r ~/.config/misc_configs/usr/* /usr/
