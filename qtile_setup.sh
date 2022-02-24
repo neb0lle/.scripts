383f0a99787c37e5a2afaba2ef0dc1862a0ddf87
NevilleJS <nevillejs24@gmail.com>
2022-02-24 09:52:43 +0530

    lockscreen stuff

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions lightdm lightdm-webkit-theme-litarvan lightdm-webkit2-greeter light-locker udiskie lxappearance gsfonts
-yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono 
+sudo pacman -S xorg kitty pulseaudio pulseaudio-alsa qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi rofi-calc zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts
+yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly
 
 chsh -s /usr/bin/zsh
 
-sudo systemctl enable lightdm
+sudo systemctl enable ly
 sudo systemctl enable udisks2
 
 sudo cp -r ~/.config/misc_configs/etc/* /etc/
