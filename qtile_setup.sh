021abd4acb7ce906d755ab037abad91d097a2b53
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-08-18 19:03:42 +0530

    bruh;

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman
+sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool
 sudo pacman -S nvidia nvidia-settings
 yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin
 
