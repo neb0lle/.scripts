eb7e393bb2b14db8c587fb3b1445da0813f05b4c
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-08-19 22:33:44 +0530

    bruh

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool
+sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli
 sudo pacman -S nvidia nvidia-settings
-yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin
+yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin stig
 
 chsh -s /usr/bin/zsh
 
 sudo systemctl enable ly
 sudo systemctl enable udisks2
 sudo systemctl enable bluetooth.service
+sudo systemctl enable transmission.service
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
