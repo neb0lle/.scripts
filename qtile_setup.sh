b49c90bc94c581702281a487c284a7d124a3bad9
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-08-22 21:09:53 +0530

    rbuh

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli
-sudo pacman -S nvidia nvidia-settings
+sudo pacman -S nvidia nvidia-settings sxiv
 yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin stig
 
 chsh -s /usr/bin/zsh
@@ -9,6 +9,5 @@ chsh -s /usr/bin/zsh
 sudo systemctl enable ly
 sudo systemctl enable udisks2
 sudo systemctl enable bluetooth.service
-sudo systemctl enable transmission.service
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
