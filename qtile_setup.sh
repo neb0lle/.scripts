a74fd28ddc41c15a4569e754a0fc9a186088284d
NevilleJS <69287021+s3dman@users.noreply.github.com>
2022-10-07 09:28:57 +0530

    bruh

--- qtile_setup.sh
+++ qtile_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo pacman -S xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli
-sudo pacman -S nvidia nvidia-settings sxiv
-yay -S picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin stig
+sudo pacman -S --noconfirm xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli
+sudo pacman -S --noconfirm nvidia nvidia-settings sxiv
+yay -S --noconfirm picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin stig
 
 chsh -s /usr/bin/zsh
 
