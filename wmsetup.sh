087a548fae5327a80a526226c280dfb953200fe3
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-02-10 23:37:35 +0530

    bruh

--- wmsetup.sh
+++ wmsetup.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-sudo pacman -S --noconfirm xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile bspwm xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl xorg-xbacklight ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli polybar
-sudo pacman -S --noconfirm nvidia nvidia-settings sxiv zerotier-one
+sudo pacman -S --noconfirm xorg kitty pipewire pipewire-pulse pipewire-alsa alsa-utils qtile bspwm xwallpaper mate-polkit sxhkd network-manager-applet dunst flameshot pulsemixer htop bashtop rofi zsh fzf starship neovim tmux python-iwlib python-psutil playerctl light ranger xclip nemo neofetch ripgrep the_silver_searcher zsh-syntax-highlighting zsh-completions udiskie lxappearance gsfonts xsecurelock xss-lock bluez bluez-utils pulseaudio-bluetooth blueman xdotool transmission-cli polybar unzip ntfs-3g pyright astyle python-pip zathura zathura-pdf-mupdf rclone ufw
+sudo pacman -S --noconfirm nvidia nvidia-settings sxiv zerotier-one gtk-engine-murrine gnome-tweaks
 yay -S --noconfirm picom-jonaburg-git rofi-greenclip nerd-fonts-jetbrains-mono ly brave-bin stig nvim-packer-git
 
 chsh -s /usr/bin/zsh
 
+sudo usermod -aG video $(whoami)
+
 sudo systemctl enable ly
 sudo systemctl enable udisks2
 sudo systemctl enable bluetooth.service
+sudo systemctl enable zerotier-one.service
+sudo systemctl enable ufw.service
+sudo systemctl enable sshd
+
+sudo systemctl start zerotier-one.service
+sudo zerotier-cli join 17d709436c897371
+
+sudo ufw allow ssh
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
